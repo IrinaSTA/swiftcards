e824ee40305f52755af529f21aba8fdc9985d105
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-10 16:41:38 +0000

    first UI test passing: text prompt to enter no. of cards per hand

--- SwiftCardsUITests/SwiftCardsUITests.swift
+++ SwiftCardsUITests/SwiftCardsUITests.swift
@@ -9,15 +9,20 @@
 import XCTest
 
 class SwiftCardsUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        
+        app = XCUIApplication()
+        
+        app.launchArguments.append("--uitesting")
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+//        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+//        XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -26,8 +31,10 @@ class SwiftCardsUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func test() {
-
+    func testHomePage() {
+        app.launch()
+        let enterHandSizeText = app.staticTexts["Please enter the number of cards per hand:"]
+        XCTAssert(enterHandSizeText.exists)
     }
 
 }
