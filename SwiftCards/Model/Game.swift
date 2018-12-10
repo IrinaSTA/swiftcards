aaff7773225ea2dacf75f605a53026bf90606ba0
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-10 17:22:44 +0000

    Add deal method to game
    Co-Authored-By: Irina <irina.baldwin@gmail.com>

--- SwiftCards/Model/Game.swift
+++ SwiftCards/Model/Game.swift
@@ -10,8 +10,17 @@ import Foundation
 class Game {
     var players: [Player]
     var handSize: Int
+    var deck: Deck = Deck()
     init(handSize: Int, players: [Player]) {
         self.players = players
         self.handSize = handSize
+    }
+    
+    func deal() {
+        for player in self.players {
+            for _ in 0..<self.handSize {
+                player.draw(deck: self.deck)
+            }
+        }
     }
 }
--- SwiftCardsTests/GameTest.swift
+++ SwiftCardsTests/GameTest.swift
@@ -19,7 +19,7 @@ class GameTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testGameHasPlayersAndHandSize() {
+    func testGameHasPlayersAndHandSizeAndDeck() {
         let player1 = Player()
         let player2 = Player()
         let playerArray = [player1, player2]
@@ -27,6 +27,18 @@ class GameTest: XCTestCase {
         XCTAssert(game.players.contains(player1))
         XCTAssert(game.players.contains(player2))
         XCTAssertEqual(game.handSize, 5)
+        XCTAssert(game.deck is Deck)
+    }
+    
+    func testGameCanDeal() {
+        let player1 = Player()
+        let player2 = Player()
+        let playerArray = [player1, player2]
+        let game = Game(handSize: 5, players: playerArray)
+        game.deal()
+        XCTAssertEqual(player1.hand.cards.count, 5)
+        XCTAssertEqual(player2.hand.cards.count, 5)
+        XCTAssertEqual(game.deck.cards.count, 42)
     }
 
     func testPerformanceExample() {
