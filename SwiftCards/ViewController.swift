20ff1b78ab36512ee386dc0f9fb6699f6beadab2
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-10 15:44:47 +0000

    Initial Commit

---
+++ SwiftCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCards
+//
+//  Created by Chris Cooksley on 10/12/2018.
+//  Copyright © 2018 Player$. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SwiftCardsTests/SwiftCardsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftCardsTests.swift
+//  SwiftCardsTests
+//
+//  Created by Chris Cooksley on 10/12/2018.
+//  Copyright © 2018 Player$. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftCards
+
+class SwiftCardsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
