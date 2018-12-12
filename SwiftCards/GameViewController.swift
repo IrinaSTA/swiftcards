aa28980ca2f4399558032a4e29fa5b48f8e187a2
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-12 17:05:56 +0000

    Now second page renders with cards drawn upon pressing play
    Co-Authored-By: Caitlin Cooling <Caitlin-cooling@users.noreply.github.com>

--- SwiftCards/GameViewController.swift
+++ SwiftCards/GameViewController.swift
@@ -9,39 +9,28 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    // MARK: - Outlets
     @IBOutlet weak var deckImage: UIImageView!
     @IBOutlet var gamePage: UIView!
-    @IBOutlet weak var handView: UICollectionView!
+    @IBOutlet weak var handView: UICollectionView! 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let game = Game(handSize: handSize, players: [player])
+        let image = UIImage(named: game.deck.cards[0].name + ".png")
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 200)
+        handView.addSubview(imageView)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    // MARK: - Action
     @IBAction func deckTapped(_ sender: Any) {
         player.draw(deck: deck)
+        print("card drawn")
     }
-    @IBAction func load(_ sender: UIButton) {
-        let game = Game(handSize: handSize, players: [player])
-        let image = UIImage(named: game.deck.cards[0].name + ".png")
-        let imageView = UIImageView(image: image!)
-        imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 200)
-        handView.addSubview(imageView)
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
