4e75acd05b59221f1f6b8b34b7f00d6ab13a744b
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-14 10:29:44 +0000

    Dragability of cards in hand
    - not printing to console
    - testing if its just my laptop
    
    Co-Authored-By: Caitlin Cooling <Caitlin-cooling@users.noreply.github.com>

--- SwiftCards/GameViewController.swift
+++ SwiftCards/GameViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 let player = Player()
 var game = Game(handSize: 5, players: [player])
 let playarea = Playarea()
-var imageView = UIImageView()
 
 class GameViewController: UIViewController {
     @IBOutlet weak var deckImage: UIImageView!
@@ -35,63 +34,49 @@ class GameViewController: UIViewController {
         if let card = player.hand.cards.last {
             render(player: player, card: card, location: handView)
         }
+        print("deck tapped")
     }
     func render(player: Player, card: Card, location: UIView) {
         if let index = player.hand.cards.index(of: card) {
             let leftPosition = index * 30
             let image = UIImage(named: card.name + ".png")
-            imageView = UIImageView(image: image!)
+            let imageView = UIImageView(image: image!)
             imageView.isAccessibilityElement = true
             imageView.accessibilityIdentifier = card.name
             imageView.frame = CGRect(x: leftPosition, y: 0, width: 90, height: 130)
             location.addSubview(imageView)
-            clickCards(imageView: imageView)
+            dragCard(imageView: imageView)
         }
     }
-    func clickCards(imageView: UIImageView) {
-            let tap = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tap:)))
-            imageView.addGestureRecognizer(tap)
-            imageView.isUserInteractionEnabled = true
-    }
-    @objc func imageTapped(tap: UITapGestureRecognizer) {
-        let tappedImage = tap.view as! UIImageView
-        getCardObject(image: tappedImage)
-    }
-    func getCardObject(image: UIImageView) {
-        player.hand.cards.forEach { card in
-            if image.accessibilityIdentifier == card.name {
-                moveCardToPlayArea(card: card, imageView: image)
-            }
-        }
-    }
-    func moveCardToPlayArea(card: Card, imageView: UIImageView) {
-        renderCardInPlayarea(player: player, card: card, location: playareaView)
-        removeCardFromHand(image: imageView)
-        let removedCard = player.hand.remove(card: card)
-        playarea.add(card: removedCard)
-        dragCard()
-    }
-    func renderCardInPlayarea(player: Player, card: Card, location: UIView) {
-        let image = UIImage(named: card.name + ".png")
-        imageView = UIImageView(image: image!)
-        imageView.isAccessibilityElement = true
-        imageView.accessibilityIdentifier = card.name
-        imageView.frame = CGRect(x: 0, y: 0, width: 90, height: 130)
-        location.addSubview(imageView)
-    }
-    func removeCardFromHand(image: UIImageView) {
-        image.removeFromSuperview()
-    }
-    func dragCard() {
+    func dragCard(imageView: UIImageView) {
         let drag = UIPanGestureRecognizer(target: self, action: #selector(pan))
         imageView.isUserInteractionEnabled = true
         imageView.addGestureRecognizer(drag)
     }
     @objc func pan(drag: UIPanGestureRecognizer) {
         print("pan")
-        let translation = drag.translation(in: imageView)
-        imageView.center.x += translation.x
-        imageView.center.y += translation.y
-        drag.setTranslation(.zero, in: imageView)
+        let touchedImage = drag.view as! UIImageView
+        let translation = drag.translation(in: touchedImage)
+        touchedImage.center.x += translation.x
+        touchedImage.center.y += translation.y
+        drag.setTranslation(.zero, in: touchedImage)
+        getCardObject(image: touchedImage)
     }
+    func getCardObject(image: UIImageView) {
+        player.hand.cards.forEach { card in
+            if image.accessibilityIdentifier == card.name {
+                updateModel(card: card, imageView: image)
+            }
+        }
+    }
+    func updateModel(card: Card, imageView: UIImageView) {
+        print(player.hand.cards.count)
+        print(playarea.cards.count)
+        let removedCard = player.hand.remove(card: card)
+        playarea.add(card: removedCard)
+        print("=======")
+        print(player.hand.cards.count)
+        print(playarea.cards.count)
+    }
+
 }
