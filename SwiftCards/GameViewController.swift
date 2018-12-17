630a12889fc524bcb077eb8d1507c98ae67869cb
Caitlin Cooling <ccooling97@gmail.com>
2018-12-17 12:30:41 +0000

    View upside down cards at the top
    - At the moment it is your own cards
    - Cards are added and removed as the game is played
    
    Co-Authored-By: Imogen Misso <imogenmisso@users.noreply.github.com>

--- SwiftCards/GameViewController.swift
+++ SwiftCards/GameViewController.swift
@@ -73,7 +73,8 @@ class GameViewController: UIViewController {
                 print(error)
             }
         }
-
+        renderHand(player.hand, location: handView)
+        renderHand(player.hand, location: opponentHandView)
     }
     @objc func imageTapped(tap: UITapGestureRecognizer) {
         let tappedImage = tap.view as! UIImageView
@@ -149,7 +150,7 @@ class GameViewController: UIViewController {
         for card in hand.cards {
             let leftPosition = Float(hand.cards.index(of: card)! * 30)
             card.setCoords(x: leftPosition, y: 0.0)
-            render(card, location: handView)
+            render(card, location: location)
         }
     }
     func renderPlayarea(_ playarea: Playarea, location: UIView) {
@@ -158,13 +159,18 @@ class GameViewController: UIViewController {
         }
     }
     func render(_ card: Card, location: UIView) {
-        let cardView = imageView(card)
+        var cardView = UIImageView()
+        if location == opponentHandView {
+           cardView = makeOpponentView(card)
+        } else {
+            cardView = makeImageView(card)
+        }
         location.addSubview(cardView)
         let xPosition = CGFloat(card.xPosition)
         let yPosition = CGFloat(card.yPosition)
         cardView.frame.origin = CGPoint(x: xPosition, y: yPosition)
     }
-    func imageView(_ card: Card) -> UIImageView {
+    func makeImageView(_ card: Card) -> UIImageView {
         let allViews = playareaView.subviews + handView.subviews
         if let existingView = allViews.first(where: {$0.accessibilityIdentifier == card.name}) {
             return existingView as! UIImageView
@@ -177,6 +183,12 @@ class GameViewController: UIViewController {
             makeTappable(imageView: imageView)
             return imageView
         }
+    }
+    func makeOpponentView(_ card: Card) -> UIImageView {
+        let image = UIImage(named: "backOfCard.png")
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 0, width: 90, height: 130)
+        return imageView
     }
     func getCardObject(image: UIImageView) -> Card {
         return Card.find(name: image.accessibilityIdentifier!)
