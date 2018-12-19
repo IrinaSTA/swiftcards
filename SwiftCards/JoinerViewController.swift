a50ad5c86c030a9936256ba00f94d6e3e0354eab
Irina Baldwin <2007inb@gmail.com>
2018-12-19 13:32:21 +0000

    Add activityIndicator to JoinerViewController; add constraints

--- SwiftCards/JoinerViewController.swift
+++ SwiftCards/JoinerViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class JoinerViewController: UIViewController {
 
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        activityIndicator.center = self.view.center
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
